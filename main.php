a31cd898c105d0a6093a5f944b02b3788717a454
Sandor <sandorgezel@gmail.com>
2024-03-28 15:59:32 +0100

    Datum formaat bij invullen, witte balk bovenaan weg

--- main.php
+++ main.php
@@ -87,10 +87,10 @@ $allMatchData = $matchCrud->actionRead();
               <?php endif; ?>
 
               <tr class="daterow">
-                <td colspan="9"><?= date_format(date_create($data["date"]), 'd/m/Y'); ?> <?= $data["time"]; ?></td>
+                <td colspan="9"><nobr><?= date_format(date_create($data["date"]), 'd/m'); ?> <?= $data["time"]; ?></nobr></td>
               </tr>
               <tr>
-                <td class="datefield"><?= date_format(date_create($data["date"]), 'd/m/Y'); ?> <?= $data["time"]; ?></td>
+                <td class="datefield"><?= date_format(date_create($data["date"]), 'd/m'); ?> <?= $data["time"]; ?></td>
                 <td><img src=".\\vlaggen\\<?= $data["home"]; ?>.png" class="flag"><?= $data["home"]; ?></td>
                 <td><?= $data["home_score"]; ?></td>
                 <td><input type="number" width="20px" class="form-control input-score" name="" value="" ?></td>
