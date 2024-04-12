c0740a9d404e1f0c01d7243b104a84562f3173a8
Sandor <sandorgezel@gmail.com>
2024-04-12 23:01:31 +0200

    Geen datafile opslaan als ongeldige json

--- action_admin_data.php
+++ action_admin_data.php
@@ -7,7 +7,23 @@ CheckAccess();
 $crud = new UserCrud();
 
 if (isset($_POST)) {
-    file_put_contents($crud->filePath, $_POST["datafile"]);
+
+    if(isJson($_POST["datafile"]))
+    {
+        file_put_contents($crud->filePath, $_POST["datafile"]);
+        $_SESSION["success_message"] = "Data file opgeslagen.";
+    }
+    else
+    {
+        $_SESSION["error_message"] = "Geen geldige json.";
+    }
+
 }
+
+
+function isJson($string) {
+    json_decode($string);
+    return json_last_error() === JSON_ERROR_NONE;
+ }
 
 header("location: admin.php?tab=data");
