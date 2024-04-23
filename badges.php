3c122069d833b489e7738a5d4dbf5552c7736088
DICA <dieter.cams@premed.be>
2024-04-23 20:41:35 +0200

    badges (finale)

--- badges.php
+++ badges.php
@@ -7,7 +7,7 @@ include("header.php");
         <div class="row">
 
             <div class="col-md-12">
-                <h1 class="blog_taital">Badges.</h1>
+                <h1 class="blog_taital">Badges</h1>
 
             </div>
             <div class="blog_section_2">
@@ -27,51 +27,125 @@ include("header.php");
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td><img src="badges\wanbetaler.png" title="Wanbetaler" width="32px" height="32px"></td>
-                                    <td>Dit is de enige badge die je letterlijk gratis krijgt en die je ook zo snel als mogelijk wil kwijtspelen. Dit is de '<b>Wanbetaler</b>'-badge en blijft staan bij iedereen die zijn bijdrage nog niet betaald heeft. Wanneer je deze badge hebt zal er ook geen score worden berekend en zal je altijd onderaan het scorebord blijven staan. (Heb je pas betaald en staat de badge er nog? Geen nood! Dit is een manuele actie die door een admin moet worden uitgevoerd en kan dus wat tijd in beslag nemen)</td>
+                                    <td>
+                                        <div class="hover-badge">
+                                            <img src="badges/wanbetaler.png" width="32" />
+                                            <span class="description">Wanbetaler</span>
+                                        </div>
+                                    </td>
+                                    <td><b>De Wanbetaler-badge</b>: Deze badge ontvang je automatisch en kosteloos, maar je wilt hem zo snel mogelijk weer kwijtraken. Deze blijft zichtbaar naast je naam zolang je contributie niet voldaan is. Zolang je deze badge hebt, wordt je score niet bijgehouden en blijf je onderaan de ranglijst staan. Heb je onlangs je bijdrage betaald maar zie je de badge nog steeds? Geen zorgen! Het verwijderen van de badge is een handmatige actie die door een beheerder uitgevoerd moet worden, wat enige tijd kan duren.</td>
                                 </tr>
                                 <tr>
-                                    <td><img src="badges\correct1.png" title="Eén wedstrijd correct" width="32px" height="32px"><img src="badges\correct5.png" title="5 wedstrijden correct" width="32px" height="32px"><img src="badges\correct10.png" title="10 wedstrijden correct" width="32px" height="32px"></td>
-                                    <td>Dit is de '<b></b>'-badge die je verdient bij het behalen van je eerste volledig correct gepronostiekeerde wedstrijd. De badge kan ook nog geüpgrade worden door het behalen van meerdere correcte uitslagen. Dit zijn de te behalen mijlpalen: <ul>
-                                            <li>1 correcte uitslag</li>
-                                            <li>5 correcte uitslagen</li>
-                                            <li>10 correcte uitslagen</li>
+                                    <td>
+                                        <div class="hover-badge">
+                                            <img src="badges/nauwkeurigevoorspeller.png" width="32" />
+                                            <span class="description">Nauwkeurige Voorspeller</span>
+                                        </div>
+                                        <div class="hover-badge">
+                                            <img src="badges/viersterrenkoning.png" width="32" />
+                                            <span class="description">Viersterrenkoning</span>
+                                        </div>
+                                        <div class="hover-badge">
+                                            <img src="badges/kampioenziener.png" width="32" />
+                                            <span class="description">Kampioen Ziener</span>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        De <strong>Voorspellingsbadges</strong>: Verdien de <strong>Nauwkeurige Voorspeller</strong>-badge wanneer je je eerste wedstrijd volledig correct voorspelt. Je kunt je badge upgraden door meer correcte uitslagen te behalen. De volgende mijlpalen wachten op je:<br /><br />
+                                        <ul>
+                                            <li><strong>1 correcte uitslag:</strong> Verdien de <strong>Nauwkeurige Voorspeller</strong>-badge.</li>
+                                            <li><strong>5 correcte uitslagen:</strong> Upgrade naar de <strong>Viersterrenkoning</strong>-badge.</li>
+                                            <li><strong>10 correcte uitslagen:</strong> Bereik de status van <strong>Kampioen Ziener</strong>.</li>
+                                        </ul><br />
+                                        Elke behaalde mijlpaal is een bewijs van je voorspellingsvaardigheden en brengt je een stap dichter bij de top van het scorebord!
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="hover-badge">
+                                            <img src="badges/pechvogel.png" width="32" />
+                                            <span class="description">Pechvogel</span>
+                                        </div>
+                                        <div class="hover-badge">
+                                            <img src="badges/Nulmeester.png" width="32" />
+                                            <span class="description">Nulmeester</span>
+                                        </div>
+                                        <div class="hover-badge">
+                                            <img src="badges/legendevandelegehand.png" width="32" />
+                                            <span class="description">Legende van de Lege Hand</span>
+                                        </div>
+                                    </td>
+                                    <td><strong>De Nulhelden-badges</strong>: Deze badges zijn voor deelnemers die wat pech hebben. Je verdient de ‘Pechvogel’-badge wanneer je een volledig foute inschatting maakt en dus 0 punten verdient bij een wedstrijd. De badge kan ook nog geüpgraded worden door het behalen van meerdere 0-scores. Dit zijn de te behalen mijlpalen:<br /><br />
+                                        <ul>
+                                            <li><strong>Géén punten verdiend bij één wedstrijd</strong>: Verdien de ‘<strong>Pechvogel</strong>’-badge.</li>
+                                            <li><strong>Géén punten verdiend bij 5 wedstrijden</strong>: Upgrade naar de ‘<strong>Nulmeester</strong>’-badge.</li>
+                                            <li><strong>Géén punten verdiend bij 10 wedstrijden</strong>: Bereik de status van ‘<strong>Legende van de Lege Hand</strong>’.</li>
+                                        </ul>
+                                        <br />
+                                        Elke mijlpaal vertelt een verhaal, zelfs als het een verhaal van nul punten is!
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="hover-badge">
+                                            <img src="badges/quickpickenthousiast.png" width="32" />
+                                            <span class="description">QuickPick Enthousiast</span>
+                                        </div>
+                                        <div class="hover-badge">
+                                            <img src="badges/quickpickgebruiker.png" width="32" />
+                                            <span class="description">QuickPick Gebruiker</span>
+                                        </div>
+                                        <div class="hover-badge">
+                                            <img src="badges/quickpickfanaat.png" width="32" />
+                                            <span class="description">QuickPick Fanaat</span>
+                                        </div>
+                                    </td>
+                                    <td>De <strong>QuickPicker-badges</strong>: Deze badges worden verdiend wanneer je ervoor kiest om het QuickPick™-systeem te gebruiken om de wedstrijden voor jou in te vullen. Je kunt deze badge ook upgraden door meerdere wedstrijden te laten invullen door QuickPick™. Hier zijn de mijlpalen die je kunt behalen:<br /><br />
+                                        <ul>
+                                            <li><strong>QuickPick Enthousiast</strong>: Laat één wedstrijd invullen door QuickPick™.</li>
+                                            <li><strong>QuickPick Gebruiker</strong>: Laat vijf wedstrijden invullen door QuickPick™.</li>
+                                            <li><strong>QuickPick Fanaat</strong>: Laat tien wedstrijden invullen door QuickPick™.</li>
                                         </ul>
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td><img src="badges\pechvogel1.png" title="Géén punten verdient bij een wedstrijd" width="32px" height="32px"><img src="badges\pechvogel5.png" title="Géén punten verdient bij 5 wedstrijden" width="32px" height="32px"><img src="badges\pechvogel10.png" title="Géén punten verdient bij 10 wedstrijden" width="32px" height="32px"></td>
-                                    <td>Dit is een badge voor de deelnemers die wat pech hebben, namelijk de '<b>Pechvogel</b>' Je krijgt deze badge namelijk wanneer je een volledig foute inschatting maakt en dus 0 punten verdient op die wedstrijd. De badge kan ook nog geüpgrade worden door het behalen van meerdere 0-scores. Dit zijn de te behalen mijlpalen: <ul>
-                                            <li>Géén punten verdient bij een wedstrijd</li>
-                                            <li>Géén punten verdient bij 5 wedstrijden</li>
-                                            <li>Géén punten verdient bij 10 wedstrijden</li>
-                                        </ul>
-                                    </td>
+                                    <td><div class="hover-badge">
+                                            <img src="badges/scoreprofeet.png" width="32" />
+                                            <span class="description">Scoreprofeet</span>
+                                        </div></td>
+                                    <td><strong>Scoreprofeet-badge</strong>: Verdien de prestigieuze Scoreprofeet-badge door jouw voorspellingsvaardigheden te tonen! Deze badge is beschikbaar zodra er <strong>drie wedstrijden</strong> gespeeld zijn. Elke deelnemer die daarna <strong>ooit de eerste plaats in de rangschikking</strong> bereikt, zal deze eretitel ontvangen en kan zich een ware Scoreprofeet noemen.</td>
                                 </tr>
                                 <tr>
-                                    <td><img src="badges\quickpick1.png" title="Eén wedstrijd laten invullen door de QuickPick&trade;" width="32px" height="32px"><img src="badges\quickpick5.png" title="5 wedstrijden laten invullen door de QuickPick&trade;" width="32px" height="32px"><img src="badges\quickpick10.png" title="10 wedstrijden laten invullen door de QuickPick&trade;" width="32px" height="32px"></td>
-                                    <td>De '<b>QuickPicker</b>'-badge krijg je wanneer je ervoor kiest om het QuickPick&trade;-systeem te gebruiken om de wedstrijden voor jou te laten invullen. De badge kan ook nog geüpgrade worden door meerdere wedstrijden te laten invullen door QuickPick&trade;. Dit zijn de te behalen mijlpalen: <ul>
-                                            <li>Eén wedstrijd laten invullen door QuickPick&trade;</li>
-                                            <li>5 wedstrijd laten invullen door QuickPick&trade;</li>
-                                            <li>10 wedstrijd laten invullen door QuickPick&trade;</li>
-                                        </ul>
-                                    </td>
+                                    <td><div class="hover-badge">
+                                            <img src="badges/onderstebovenkampioen.png" width="32" />
+                                            <span class="description">Onderstebovenkampioen</span>
+                                        </div></td>
+                                    <td><strong>Onderstebovenkampioen-badge</strong>: Word gekroond tot Onderstebovenkampioen! Deze unieke badge is beschikbaar na het spelen van <strong>drie wedstrijden</strong>. Elke deelnemer die daarna <strong> de laatste plaats in de rangschikking</strong> inneemt, zal de Onderstebovenkampioen-badge ontvangen. Het is een speelse erkenning voor het ervaren van een onverwachte wending in het spel!</td>
                                 </tr>
                                 <tr>
-                                    <td><img src="badges\winnaar.png" title="Ik ben de beste!" width="32px" height="32px"></td>
-                                    <td>Dit is de '<b>Topspeler</b>-badge. Deze kan je verdienen vanaf het moment dat er 3 wedstrijden gespeeld zijn. Iedereen die vanaf dan ooit op de eerste plaats in de rangschikking komt te staan, verdient deze badge.</td>
+                                    <td><div class="hover-badge">
+                                            <img src="badges/puntenkoning.png" width="32" />
+                                            <span class="description">Puntenkoning</span>
+                                        </div></td>
+                                    <td><strong>Puntenkoning-badge</strong>: Kroon jezelf tot Puntenkoning! Deze badge is jouw trofee voor het behalen van een indrukwekkende score. Je verdient deze badge wanneer je in <strong>drie opeenvolgende wedstrijden</strong> een totaal van <strong>meer dan 6 punten</strong> behaalt. Laat zien dat je een meester bent in het voorspellen en verzamel de punten om deze koninklijke onderscheiding te claimen!</td>
                                 </tr>
                                 <tr>
-                                    <td><img src="badges\laatste.png" title="Laatste" width="32px" height="32px"></td>
-                                    <td>De '<b>Flopspeler</b>'-badge kan je krijgen vanaf het moment dat er 3 wedstrijden gespeeld zijn. Iedereen die vanaf dan ooit op de laatste plaats in de rangschikking komt te staan, krijgt deze badge.</td>
+                                    <td><div class="hover-badge">
+                                            <img src="badges/vragenvirtuoos.png" width="32" />
+                                            <span class="description">Vragenvirtuoos</span>
+                                        </div></td>
+                                    <td><strong>Vragenvirtuoos-badge</strong>: Toon je kennis en word een Vragenvirtuoos! Deze badge is een eerbetoon aan jouw scherpe geest en snelle denkvermogen. Je verdient de Vragenvirtuoos-badge door <strong>minstens de helft van de vragen</strong> in de vragenronde <strong>correct te beantwoorden</strong>. Het is een bewijs van jouw vermogen om onder druk te presteren en de uitdaging van de quiz met succes aan te gaan!</td>
                                 </tr>
                                 <tr>
-                                    <td><img src="badges\puntenkoning.png" title="Puntenkoning" width="32px" height="32px"></td>
-                                    <td>Dit is de '<b>Puntenkoning</b>'-badge. Deze verdien je wanneer de som van de behaalde punten over 3 opeenvolgende wedstrijden het totaal van 6 punten overschrijdt. </td>
+                                    <td><div class="hover-badge">
+                                            <img src="badges/ekexpert.png" width="32" />
+                                            <span class="description">EK Expert</span>
+                                        </div></td>
+                                    <td><strong>EK Expert-badge</strong>: De ultieme uitdaging wacht op je! De EK Expert-badge is de kroon op jouw voorspellingskunsten. Deze <strong>exclusieve onderscheiding</strong> is voor degenen die hun inzicht en strategie bewijzen door de kaap van <strong>100 punten te behalen</strong> gedurende het gehele Europees Kampioenschap. Het is een ware test van je kennis en toewijding aan de sport. Verover deze badge en laat zien dat je een echte EK Expert bent!</td>
                                 </tr>
                             </tbody>
                         </table>
-                        
+
 
                     </div>
                 </div>
