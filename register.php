97e818f136ba8b195315c0c3d19d4bdaf55a9960
Sandor <sandorgezel@gmail.com>
2024-05-21 15:00:46 +0200

    Registreren: velden zijn verplicht in te vullen

--- register.php
+++ register.php
@@ -5,32 +5,28 @@ include_once("UserCrud.php");
 $crud = new UserCrud();
 ?>
 
-
-
 <form method="post" action="action_register.php" autocomplete="off">
-<div class="contact_section layout_padding">
-
-   
-         <div class="container-fluid">
-            <div class="row">
-               <div class="col-md-6">
-                  <div class="mail_section_1">
-                     <h1 class="contact_taital">Registreren</h1>
-                     <div class="form">
-                        <input type="text" class="mail_text" placeholder="Email" name="username"  autofill="false" />
-                        <input type="text" class="mail_text" placeholder="Naam" name="name" />
-                        <input type="password" class="mail_text" placeholder="Wachtwoord" name="password" />                       
-                        <input type="password" class="mail_text" placeholder="Wachtwoord herhalen" name="password2" />                       
-                     </div>
-                     <input type="submit" class="btn btn-primary" value="Maak account" />
+   <div class="contact_section layout_padding">
+      <div class="container-fluid">
+         <div class="row">
+            <div class="col-md-6">
+               <div class="mail_section_1">
+                  <h1 class="contact_taital">Registreren</h1>
+                  <div class="form">
+                     <input type="email" class="mail_text" placeholder="Email" name="username" required autofill="false" />
+                     <input type="text" class="mail_text" placeholder="Naam" name="name" required/>
+                     <input type="password" class="mail_text" placeholder="Wachtwoord" name="password" required/>
+                     <input type="password" class="mail_text" placeholder="Wachtwoord herhalen" name="password2" required/>
                   </div>
+                  <input type="submit" class="btn btn-primary" value="Maak account" />
                </div>
-               <div class="col-md-6 padding_right_0">
-                  <div class="contact_img"><img src="images/contact-img.png"></div>
-               </div>
+            </div>
+            <div class="col-md-6 padding_right_0">
+               <div class="contact_img"><img src="images/contact-img.png"></div>
             </div>
          </div>
       </div>
+   </div>
 </form>
 
 <?php
